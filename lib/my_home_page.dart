b902a847aafe3905fe58c3e413b3b261f2dd8ff4
Gabriel S. Souza <gabriel12saraiva@gmail.com>
2022-03-12 13:49:24 -0300

    Implemented InheritedWidget/Notifier

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -1,49 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_teste_1/home_controller.dart';
 
-// class MyHomePage extends StatelessWidget {
-//   const MyHomePage({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     // Isto busca na árvore o HomeControler. Toda vez que o notifier.value for alterdo,
-//     // devido ao dependOnInherited... , o retorno deste método build será atualizado
-//     // automaticamente, o que confere reatividade a este Widget. Um controle de estado está sendo feito
-//     var controller = HomeController.of(context);
-
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text("Gabriel"),
-//         backgroundColor: Colors.amber,
-//       ),
-//       body: Center(
-//           child: Text(
-//         "Você clicou no botão ${controller.value} vezes",
-//         textScaleFactor: 2,
-//       )),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: () {
-//           controller.increment();
-//         },
-//         backgroundColor: Colors.amber,
-//         child: const Icon(Icons.add),
-//       ),
-//     );
-//   }
-// }
-
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  var counter = 0;
-
-  @override
   Widget build(BuildContext context) {
+    // Isto busca na árvore o HomeControler. Toda vez que o notifier.value for alterdo,
+    // devido ao dependOnInherited... , o retorno deste método build será atualizado
+    // automaticamente, o que confere reatividade a este Widget. Um controle de estado está sendo feito
+    var controller = HomeController.of(context);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Gabriel"),
@@ -51,15 +18,12 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
           child: Text(
-          "Você clicou no botão $counter vezes",
-          textScaleFactor: 2,
-        )
-      ),
+        "Você clicou no botão ${controller.value} vezes",
+        textScaleFactor: 2,
+      )),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          setState(() {
-            counter++;
-          });
+          controller.increment();
         },
         backgroundColor: Colors.amber,
         child: const Icon(Icons.add),
@@ -67,3 +31,39 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({Key? key}) : super(key: key);
+
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   var counter = 0;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text("Gabriel"),
+//         backgroundColor: Colors.amber,
+//       ),
+//       body: Center(
+//           child: Text(
+//           "Você clicou no botão $counter vezes",
+//           textScaleFactor: 2,
+//         )
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: () {
+//           setState(() {
+//             counter++;
+//           });
+//         },
+//         backgroundColor: Colors.amber,
+//         child: const Icon(Icons.add),
+//       ),
+//     );
+//   }
+// }
