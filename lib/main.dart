acf0ec97e0ccbd1029c999aaacb66e9db88ff52d
Gabriel S. Souza <gabriel12saraiva@gmail.com>
2022-03-12 14:37:56 -0300

    Adjusted for implement Column and Row Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,54 @@
 import 'package:flutter/material.dart';
-
-import 'my_app.dart';
-
 void main() {
   runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // StatelessWidget: Widget sem estado
+  const MyApp({Key? key}) : super(key: key);
+
+  // Este widget é a raiz do seu aplicativo.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      // Utilizando o InheritedWidget
+      home:  MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Gabriel"),
+        backgroundColor: Colors.amber,
+      ),
+      body: Center(
+          child: Text(
+          "Você clicou no botão $counter vezes",
+          textScaleFactor: 2,
+        )
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            counter++;
+          });
+        },
+        backgroundColor: Colors.amber,
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
 }
