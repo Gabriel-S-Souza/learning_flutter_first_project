84571abef539f26ae0f2449b38d6944dedfd6e7c
Gabriel S. Souza <gabriel12saraiva@gmail.com>
2022-03-12 14:25:21 -0300

    Remove comments

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -31,39 +31,3 @@ class MyHomePage extends StatelessWidget {
     );
   }
 }
-
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({Key? key}) : super(key: key);
-
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   var counter = 0;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text("Gabriel"),
-//         backgroundColor: Colors.amber,
-//       ),
-//       body: Center(
-//           child: Text(
-//           "Você clicou no botão $counter vezes",
-//           textScaleFactor: 2,
-//         )
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: () {
-//           setState(() {
-//             counter++;
-//           });
-//         },
-//         backgroundColor: Colors.amber,
-//         child: const Icon(Icons.add),
-//       ),
-//     );
-//   }
-// }
